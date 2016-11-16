92161152f095654ec9e3696fdadd3e5e6edbf444
Tô Thành Công <tcvn1985@gmail.com>
2016-11-16 21:54:15 +0700

    fix dirname
    fix dirname

--- OPS-U16/scripts/install/install_keystone.sh
+++ OPS-U16/scripts/install/install_keystone.sh
@@ -3,6 +3,9 @@
 
 ###############################################################################
 ## Khai bao cac chuong trinh ho tro
+dir_path=$(dirname $0)
+source $dir_path/config.cfg
+source $dir_path/functions.sh
 
 echocolor "Create Database for Keystone"
 
--- OPS-U16/scripts/setup03.sh
+++ OPS-U16/scripts/setup03.sh
@@ -3,12 +3,25 @@
 
 ###############################################################################
 ## Khai bao cac chuong trinh ho tro
-dir_path=$(dirname $0)
+dir_path=$(pwd $0)
 source $dir_path/config.cfg
 source $dir_path/functions.sh
+
+### Running function
+### Checking and help syntax command
+if [ $# -ne 1 ]
+    then
+        echocolor  "Syntax command "
+        echo "Syntax command on Controller: bash $0 controller"
+        echo "Syntax command on Compute1: bash $0 compute1"
+        echo "Syntax command on Compute2: bash $0 compute2"
+        exit 1;
+fi
 
 if [ "$1" == "controller" ]; then
 	bash $dir_path/install/install_keystone.sh
 else
 	echocolor "Xin chao"
 	exit 1;
+
+fi
