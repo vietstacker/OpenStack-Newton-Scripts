edcb719d5dbcb578a217f8e2261b063b640c6ae3
Tô Thành Công <tcvn1985@gmail.com>
2016-11-16 16:26:22 +0700

    cap nhat script

--- OPS-U16/scripts/install-openstack.sh
+++ OPS-U16/scripts/install-openstack.sh
@@ -10,22 +10,22 @@ source functions.sh
 ## Khai bao duong dan
 path_chrony=/etc/chrony/chrony.conf
 
-#############################################
-function install_crudini {
+function install_crudini ()
+{
 	echocolor "Installing CRUDINI"
 	sleep 3
 	apt-get -y install crudini
 }
 
-#############################################
-function install_python_client {
+function install_python_client () 
+{
 	echocolor "Install python client"
 	sleep 3
 	apt-get -y install python-openstackclient
 }
 
-#############################################
-function install_ntp {
+function install_ntp () 
+{
 	echocolor "Install and config NTP"
 	sleep 3
 
@@ -58,33 +58,27 @@ function install_ntp {
 		
 }
 
-#############################################
-function install_database  {
+function install_database () 
+{
 	echocolor "Install MYSQL"
 	sleep 3
 
-	echo mariadb-server-10.0 mysql-server/root_password $MYSQL_PASS | \
-        debconf-set-selections
-	echo mariadb-server-10.0 mysql-server/root_password_again $MYSQL_PASS | \
-        debconf-set-selections
+	echo mariadb-server-10.0 mysql-server/root_password $MYSQL_PASS | debconf-set-selections
+	echo mariadb-server-10.0 mysql-server/root_password_again $MYSQL_PASS | debconf-set-selections
+	apt-get install -y  mariadb-server
 
-    apt-get install -y  mariadb-server
-
-	sed -r -i 's/127\.0\.0\.1/0\.0\.0\.0/' \
-        /etc/mysql/mariadb.conf.d/50-server.cnf
-	sed -i 's/character-set-server  = utf8mb4/character-set-server  = utf8/' \
-        /etc/mysql/mariadb.conf.d/50-server.cnf
-	sed -i 's/collation-server/#collation-server/'  \
-        /etc/mysql/mariadb.conf.d/50-server.cnf
+	sed -r -i 's/127\.0\.0\.1/0\.0\.0\.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+	sed -i 's/character-set-server  = utf8mb4/character-set-server  = utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf
+	sed -i 's/collation-server/#collation-server/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 	systemctl restart mysql
 
-	mysql -uroot -p$MYSQL_PASS
+	cat << EOF | mysql -uroot -p$MYSQL_PASS 
 	GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION;
 	GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION;
 	FLUSH PRIVILEGES;
-	exit;
-	
+	EOF
+ 	
 	ops_edit /etc/mysql/conf.d/openstack.cnf  client default-character-set utf8
 	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld bind-address 0.0.0.0
 	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld default-storage-engine innodb
@@ -99,8 +93,11 @@ function install_database  {
 
 }
 
+
 #############################################
-function install_rabbitmq {
+
+function install_rabbitmq ()
+{
 	echocolor "Install and Config RabbitMQ"
 	sleep 3
 
@@ -115,7 +112,8 @@ function install_rabbitmq {
 }
 
 ##############################################
-function install_memcache {
+function install_memcache ()
+{
 	echocolor "Install memcache"
 	sleep 3
 	apt-get -y install memcached python-memcache
@@ -130,9 +128,9 @@ function install_memcache {
 ### Kie tra ham va cu phap thuc thi 
 if [ $# -ne 1 ]; then
         echocolor  "Thu thi voi cac tuy chon sau"
-        echo "Thuc hien tren Controller:  "bash $0 controller" "
-        echo "Thuc hien tren Compute1:  "bash $0 compute1" "
-        echo "Thuc hien tren Compute1: "bash $0 compute2" "
+        echo "Thuc hien tren Controller bash $0 controller"
+        echo "Thuc hien tren Compute1 bash $0 compute1"
+        echo "Thuc hien tren Compute1 bash $0 compute2"
         exit 1;
 fi
 
