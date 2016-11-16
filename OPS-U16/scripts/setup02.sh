fa8eb5fee78a867f73b979eb053332b76accb4bd
Tô Thành Công <tcvn1985@gmail.com>
2016-11-16 17:39:40 +0700

    fix script 02

--- OPS-U16/scripts/setup02.sh
+++ OPS-U16/scripts/setup02.sh
@@ -9,6 +9,8 @@ source functions.sh
 ###############################################################################
 ## Khai bao duong dan
 path_chrony=/etc/chrony/chrony.conf
+path_db_openstack=/etc/mysql/conf.d/openstack.cnf
+path_db_50server=/etc/mysql/mariadb.conf.d/50-server.cnf
 
 #############################################
 function install_crudini {
@@ -58,40 +60,39 @@ function install_ntp {
 		
 }
 
-#############################################
-function install_database  {
+###############################################################################
+function install_database ()
+{
 	echocolor "Install MYSQL"
 	sleep 3
 
 	echo mariadb-server-10.0 mysql-server/root_password $MYSQL_PASS | \
-        debconf-set-selections
+	    debconf-set-selections
 	echo mariadb-server-10.0 mysql-server/root_password_again $MYSQL_PASS | \
-        debconf-set-selections
+	    debconf-set-selections
 
-    apt-get install -y  mariadb-server
+	apt-get install -y  mariadb-server
 
-	sed -r -i 's/127\.0\.0\.1/0\.0\.0\.0/' \
-        /etc/mysql/mariadb.conf.d/50-server.cnf
+	sed -r -i 's/127\.0\.0\.1/0\.0\.0\.0/' $path_db_50server
 	sed -i 's/character-set-server  = utf8mb4/character-set-server  = utf8/' \
-        /etc/mysql/mariadb.conf.d/50-server.cnf
-	sed -i 's/collation-server/#collation-server/'  \
-        /etc/mysql/mariadb.conf.d/50-server.cnf
+	    $path_db_50server
+	sed -i 's/collation-server/#collation-server/'  $path_db_50server
 
 	systemctl restart mysql
 
-	mysql -uroot -p$MYSQL_PASS
-	GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION;
-	GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION;
-	FLUSH PRIVILEGES;
-	exit;
+	cat << EOF | mysql -uroot -p$MYSQL_PASS 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASS' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 	
-	ops_edit /etc/mysql/conf.d/openstack.cnf  client default-character-set utf8
-	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld bind-address 0.0.0.0
-	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld default-storage-engine innodb
-	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld innodb_file_per_table
-	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld max_connections 4096
-	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld collation-server utf8_general_ci
-	ops_edit /etc/mysql/conf.d/openstack.cnf  mysqld character-set-server utf8
+	ops_edit $path_db_openstack client default-character-set utf8
+	ops_edit $path_db_openstack mysqld bind-address 0.0.0.0
+	ops_edit $path_db_openstack mysqld default-storage-engine innodb
+	ops_edit $path_db_openstack mysqld innodb_file_per_table
+	ops_edit $path_db_openstack mysqld max_connections 4096
+	ops_edit $path_db_openstack mysqld collation-server utf8_general_ci
+	ops_edit $path_db_openstack mysqld character-set-server utf8
 
 	echocolor "Restarting MYSQL"
 	sleep 5
@@ -99,7 +100,7 @@ function install_database  {
 
 }
 
-#############################################
+###############################################################################
 function install_rabbitmq {
 	echocolor "Install and Config RabbitMQ"
 	sleep 3
@@ -114,7 +115,7 @@ function install_rabbitmq {
 	echocolor "Finish setup pre-install package !!!"
 }
 
-##############################################
+###############################################################################
 function install_memcache {
 	echocolor "Install memcache"
 	sleep 3
@@ -125,14 +126,14 @@ function install_memcache {
 	echocolor "Done, you can run next script"
 }
 
-### Thuc thi ham
-
-### Kie tra ham va cu phap thuc thi 
-if [ $# -ne 1 ]; then
-        echocolor  "Thu thi voi cac tuy chon sau"
-        echo "Thuc hien tren Controller:  "bash $0 controller" "
-        echo "Thuc hien tren Compute1:  "bash $0 compute1" "
-        echo "Thuc hien tren Compute1: "bash $0 compute2" "
+### Running function
+### Checking and help syntax command
+if [ $# -ne 1 ]
+    then
+        echocolor  "Syntax command "
+        echo "Syntax command on Controller: bash $0 controller"
+        echo "Syntax command on Compute1: bash $0 compute1"
+        echo "Syntax command on Compute2: bash $0 compute2"
         exit 1;
 fi
 
