14f29e671f4946a106a48452159fce79eb3d7d58
Hieu LE <hieulq19@gmail.com>
2016-11-17 15:23:33 +0900

    Create bashate.sh

---
+++ tools/bashate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Executing Bashate (https://github.com/openstack-dev/bashate)"
+#if bashate sample_script.sh; then
+#    echo "Bashate exited without errors."
+#fi
+bash -c "grep -Irl \
+        -e '!/usr/bin/env bash' \
+        -e '!/bin/bash' \
+        -e '!/bin/sh' \
+        --exclude-dir '.*' \
+        --exclude-dir 'DOCs-OPS-Newton' \
+        --exclude-dir 'RDO-OpenStack-Newton-Guide' \
+        --exclude-dir '*.egg-info' \
+        --exclude 'bashate.sh' \
+        --exclude '*.md' \
+        --exclude '*.txt' \
+        ../ | xargs bashate --verbose"
